ae356f1c6b9044ce20a841833b73dcda63f7c276
SR <e.saad.kinan@gmail.com>
2023-12-07 00:26:39 +0100

    final v

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend-service';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, NgModule} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-
+import {provideHttpClient} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient(), FormsModule]
 };
